b3fa47480f97ec7beb40abbe3da38cc2fcb7b8a7
FlutLab.io <>
2022-05-02 18:21:00 +0000

    Autosave

---
+++ lib/gridviewarticulos.dart
@@ -0,0 +1,43 @@
+import 'package:flutter/material.dart';
+
+class PaginaInicial extends StatefulWidget {
+  const PaginaInicial({Key? key}) : super(key: key);
+  @override
+  _PaginaInicialState createState() => _PaginaInicialState();
+} // Widget con estado
+
+class _PaginaInicialState extends State<PaginaInicial> {
+  List<String> images = [
+    "assets/images/perrito.jpg",
+    "assets/images/cirugia.jpg",
+    "assets/images/veterinaria.jpg",
+    "assets/images/veterinario.jpg",
+    "assets/images/images.jpg",
+    "assets/images/8.jpg",
+    "assets/images/licencia.jpg",
+    "assets/images/veterinarian.jpg",
+  ]; // Lista de imagenes
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: const Text("Flutter GridView"),
+        ),
+        body: GridView.custom(
+          gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+            crossAxisCount: 2,
+          ),
+          childrenDelegate: SliverChildBuilderDelegate(
+            (BuildContext, index) {
+              return Image.asset(
+                images[index],
+                fit: BoxFit.cover,
+              );
+            },
+            childCount: 8,
+          ),
+          padding: const EdgeInsets.all(10),
+          shrinkWrap: true,
+        ));
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:castaneda/gridviewarticulos.dart';
 
 void main() {
-  runApp(MyApp());
+  runApp(VeterinariaApp());
 }
 
-class MyApp extends StatelessWidget {
+class VeterinariaApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -13,48 +14,6 @@ class MyApp extends StatelessWidget {
         theme: ThemeData(
           primarySwatch: Colors.green,
         ), // Tema color global
-        home: const MyHomePage());
+        home: const PaginaInicial());
   }
 } // Widget sin estado
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({Key? key}) : super(key: key);
-  @override
-  _MyHomePageState createState() => _MyHomePageState();
-} // Widget con estado
-
-class _MyHomePageState extends State<MyHomePage> {
-  List<String> images = [
-    "assets/images/perrito.jpg",
-    "assets/images/cirugia.jpg",
-    "assets/images/veterinaria.jpg",
-    "assets/images/veterinario.jpg",
-    "assets/images/images.jpg",
-    "assets/images/8.jpg",
-    "assets/images/licencia.jpg",
-    "assets/images/veterinarian.jpg",
-  ]; // Lista de imagenes
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-        appBar: AppBar(
-          title: const Text("Flutter GridView"),
-        ),
-        body: GridView.custom(
-          gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-            crossAxisCount: 2,
-          ),
-          childrenDelegate: SliverChildBuilderDelegate(
-            (BuildContext, index) {
-              return Image.asset(
-                images[index],
-                fit: BoxFit.cover,
-              );
-            },
-            childCount: 8,
-          ),
-          padding: const EdgeInsets.all(10),
-          shrinkWrap: true,
-        ));
-  }
-}
